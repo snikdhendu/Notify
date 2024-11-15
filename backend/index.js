d6aa4b7c6e05bb7af7a4f508f52eca6282f44b11
snikdhendu <snikdhendupramanik@gmail.com>
2024-11-15 12:50:19 +0530

    frontend link added

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,34 @@
-const express = require("express")
-const cors = require("cors")
-const mongoose = require("mongoose")
-const dotenv = require("dotenv")
-// const bodyParser = require("body-parser")
-const app = express()
-const Routes = require("./routes/route.js")
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = express();
+const Routes = require("./routes/route.js");
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
-// app.use(bodyParser.json({ limit: '10mb', extended: true }))
-// app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
+const corsOptions = {
+  origin: 'https://notify-swe.vercel.app', // Allow only this domain
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  credentials: true, // If you need to send cookies or headers
+};
 
-app.use(express.json({ limit: '10mb' }))
-app.use(cors())
+app.use(cors(corsOptions)); // Use the corsOptions with specific origin
+
+app.use(express.json({ limit: '10mb' }));
 
 mongoose
-    .connect(process.env.MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(console.log("Connected to MongoDB"))
-    .catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("NOT CONNECTED TO NETWORK", err));
 
-app.use('/', Routes);
+app.use("/", Routes);
 
 app.listen(PORT, () => {
-    console.log(`Server started at port no. ${PORT}`)
-})
+  console.log(`Server started at port no. ${PORT}`);
+});
